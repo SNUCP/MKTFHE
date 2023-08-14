d5efe3afdd27b30294afc73643849fb761259254
Seonhong Min (Luke Min) <100746641+snu-lukemin@users.noreply.github.com>
2023-08-14 11:20:48 +0900

    Quick Fix on Barrett reduction

--- src/ring/arithmetic.jl
+++ src/ring/arithmetic.jl
@@ -2,12 +2,9 @@
     if x == .0
         0x00000000
     else
+        x = round(x)
         x -= floor(x * 2.3283064365386963e-10) * 4.294967296e9
-        if x == 4.294967296e9
-            0x00000000
-        else
-            round(UInt32, x)
-        end
+        round(UInt32, x)
     end
 end
 
@@ -15,12 +12,9 @@ end
     if x == .0
         0x0000000000000000
     else
+        x = round(x)
         x -= floor(x * 5.421010862427522e-20) * 1.8446744073709552e19
-        if x == 1.8446744073709552e19
-            0x0000000000000000
-        else
-            round(UInt64, x)
-        end
+        round(UInt64, x)
     end
 end
 
